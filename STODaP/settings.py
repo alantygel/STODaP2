cec6ebc87f3b1397016590613429fee80699c397
Alan Tygel <alantygel@ppgi.ufrj.br>
2016-05-09 08:01:27 -0300

    evaluation framework

--- STODaP/settings.py
+++ STODaP/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tag_analytics.apps.TagAnalyticsConfig',
+    'evaluation',#.apps.EvaluationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- STODaP/urls.py
+++ STODaP/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^evaluation/', include('evaluation.urls')),
     url(r'^tag_analytics/', include('tag_analytics.urls')),
     url(r'^admin/', admin.site.urls),
 ]
