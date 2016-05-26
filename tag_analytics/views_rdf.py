b530ce461a6d9b648984f0865d1c400178f5b3d9
Alan Tygel <alantygel@ppgi.ufrj.br>
2016-05-26 13:11:25 -0300

    abs url for rdf root

--- tag_analytics/views_rdf.py
+++ tag_analytics/views_rdf.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse,Http404
 from django.shortcuts import render
 from django.template import loader
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
 
 from time import time
 
@@ -21,62 +22,73 @@ def VocabularyRDFView(request):
 
 def SemanticTagRdfDetailView(request,pk):
 	template = loader.get_template('tag_analytics/rdf/semantictag.rdf')
-	context = { 'semantictag' : GlobalTag.objects.get(pk = pk)}
+	context = { 'semantictag' : GlobalTag.objects.get(pk = pk),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def TagRdfDetailView(request,pk):
 	template = loader.get_template('tag_analytics/rdf/tag.rdf')
-	context = { 'tag' : Tag.objects.get(pk = pk)}
+	context = { 'tag' : Tag.objects.get(pk = pk),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def GroupRdfDetailView(request,pk):
 	template = loader.get_template('tag_analytics/rdf/group.rdf')
-	context = { 'group' : Group.objects.get(pk = pk)}
+	context = { 'group' : Group.objects.get(pk = pk),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def DatasetRdfDetailView(request,pk):
 	template = loader.get_template('tag_analytics/rdf/dataset.rdf')
-	context = { 'dataset' : Dataset.objects.get(pk = pk)}
+	context = { 'dataset' : Dataset.objects.get(pk = pk),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def SemanticGroupRdfDetailView(request,pk):
 	template = loader.get_template('tag_analytics/rdf/semanticgroup.rdf')
-	context = { 'semanticgroup' : GlobalGroup.objects.get(pk = pk)}
+	context = { 'semanticgroup' : GlobalGroup.objects.get(pk = pk),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def OpenDataPortalRdfDetailView(request,pk):
 	template = loader.get_template('tag_analytics/rdf/opendataportal.rdf')
-	context = { 'opendataportal' : OpenDataPortal.objects.get(pk = pk)}
+	context = { 'opendataportal' : OpenDataPortal.objects.get(pk = pk),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def SemanticTagRdfListView(request):
 	template = loader.get_template('tag_analytics/rdf/semantictags.rdf')
-	context = { 'semantictag_list' : GlobalTag.objects.all()}
+	context = { 'semantictag_list' : GlobalTag.objects.all(),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def TagRdfListView(request):
 	template = loader.get_template('tag_analytics/rdf/tags.rdf')
-	context = { 'tag_list' : Tag.objects.all()}
+	context = { 'tag_list' : Tag.objects.all(),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def GroupRdfListView(request):
 	template = loader.get_template('tag_analytics/rdf/groups.rdf')
-	context = { 'group_list' : Group.objects.all()}
+	context = { 'group_list' : Group.objects.all(),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def DatasetRdfListView(request,start=0,step=50):
 	template = loader.get_template('tag_analytics/rdf/datasets.rdf')
 	end = int(start) + int(step)
-	print end
-	context = { 'dataset_list' : Dataset.objects.all()[start:end]}
+	context = { 'dataset_list' : Dataset.objects.all()[start:end],
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def SemanticGroupRdfListView(request):
 	template = loader.get_template('tag_analytics/rdf/semanticgroups.rdf')
-	context = { 'semanticgroup_list' : GlobalGroup.objects.all()}
+	context = { 'semanticgroup_list' : GlobalGroup.objects.all(),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
 
 def OpenDataPortalRdfListView(request):
 	template = loader.get_template('tag_analytics/rdf/opendataportals.rdf')
-	context = { 'opendataportal_list' : OpenDataPortal.objects.all()}
+	context = { 'opendataportal_list' : OpenDataPortal.objects.all(),
+				'vocab_host' : settings.VOCAB_HOST}
 	return HttpResponse(template.render(context,request), content_type = 'application/rdf+xml')
