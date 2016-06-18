e7cf728a1cc0d24c210488655487faf5c1e94c4d
Alan Tygel <alantygel@ppgi.ufrj.br>
2016-06-18 20:45:33 -0300

    finish evaluation

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
 	url(r'^start/$', views.index, name='index'),
 	url(r'^initialize/$', views.initialize, name='initialize'),
 	url(r'^finish/$', views.finish, name='finish'),
